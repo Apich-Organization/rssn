993f60b2fb2f9fe3c9cf0f48193522c807e36f97
Xinyu Yang <Pana.Yang@hotmail.com>
2026-08-08 11:04:58 +0800

    bump: 0.2.13

--- build.rs
+++ build.rs
@@ -12,6 +12,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("cargo:rustc-env=VERGEN_SYSINFO_KERNEL_VERSION=unknown");
 
+    println!("cargo:rustc-env=VERGEN_SYSINFO_KERNEL_VERSION=unknown");
+    println!("cargo:rustc-env=VERGEN_GIT_BRANCH=unknown");
+    println!("cargo:rustc-env=VERGEN_GIT_COMMIT_AUTHOR_EMAIL=unknown");
+    println!("cargo:rustc-env=VERGEN_GIT_COMMIT_AUTHOR_NAME=unknown");
+    println!("cargo:rustc-env=VERGEN_GIT_COMMIT_MESSAGE=unknown");
+    println!("cargo:rustc-env=VERGEN_GIT_DESCRIBE=unknown");
+    println!("cargo:rustc-env=VERGEN_GIT_SHA=unknown");
+
     let mut emitter = Emitter::default();
 
     let build = Build::all_build();
@@ -20,7 +28,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let rustc = Rustc::all_rustc();
     let cargo = Cargo::all_cargo();
     emitter
-        .add_instructions(&gitcl)?
+        //.add_instructions(&gitcl)?
         .add_instructions(&build)?
         .add_instructions(&si)?
         .add_instructions(&rustc)?
